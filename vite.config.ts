753c2342273e3e5863a8b72978bf2a978fa41c08
Hoang Trang <hoangtrang@example.com>
2026-07-07 22:21:44 +0700

    fix CSS path base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/FamilyFinBot/',
+    base: "/FamilyFinBot/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
